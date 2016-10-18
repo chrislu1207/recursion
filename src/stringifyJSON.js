db7361a08fdb350a126c15eda04829e1c5b41183
Chris <krizloo@gmail.com>
2016-10-18 16:09:17 -0700

    Made tests pass for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,42 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) { // Case where object is null
+    return "null";
+  } else if (typeof(obj) === 'string') { // Case where object is of type string
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) { // Case where object is of type array
+    if (obj.length) {
+      var JSONstring = [];
+      for (var i = 0; i < obj.length; i++) {
+        JSONstring.push(stringifyJSON(obj[i]));
+      }
+      return '[' + JSONstring.join(",") + ']';
+    } else {
+      return '[]';
+    }
+  } else if (typeof(obj) === 'object') { // Case where object is of type object
+    var keys = Object.keys(obj);
+    var endOfObject = keys.length - 1;
+    if (keys.length) {
+      var JSONstring = '';
+      for (var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+        if (obj[key] === undefined || typeof key === 'function' || typeof obj[key] === 'function') {
+          // Do nothing
+        } else {
+          if (i === endOfObject) {
+            JSONstring += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+          } else {
+            JSONstring += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+          }
+        }
+      }
+      return '{' + JSONstring + '}';
+    } else {
+      return '{}';
+    }
+  } else { // Case where object is boolean or number
+    return obj.toString();
+  }
 };
